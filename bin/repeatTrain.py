eb6538288b3f5e13df25dfe333b9f00b5256e740
dougnd <dougnd@gmail.com>
2016-11-04 16:18:08 -0400

    add repeat Train (initial version)

--- bin/repeatTrain.py
+++ bin/repeatTrain.py
@@ -1,16 +1,54 @@
 import vtpalmetto
-from sh import rm
+from sh import rm, Command, cp
+import argparse
 
+vtp = vtpalmetto.VTPalmetto()
+vtp.qsubParams = dict(l='select=1:ncpus=1:mem=16gb:ngpus=1:gpu_model=k40,walltime=20:00:00')
+vtp.name = 'repeatTrain'
 
-def task(**kwarg):
-    print 'hello world'
-    vtpalmetto.gotoTmp()
+testFrames = [10, 11, 12, 13, 14]
+trainFrames = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+x=0
+y=0
+w=5300
+h=3500
+sz=300
+n=20
+
+def task(dataset):
+    vtp.gotoTmp()
     rm('-rf', 'vehicleTracking')
-    vtpalmetto.getVT()
-    vtpalmetto.cmakeVT()
-    vtpalmetto.makeVT()
-    print 'hello world2'
+    vtp.getVT()
+    vtp.cmakeVT()
 
-vtpalmetto.name = 'repeatTrain'
-vtpalmetto.main(task, [dict(), dict()])
+    vtp.makeVT('labeledDataToDB')
+    labeledDataToDB = Command("util/labeledDataToDB")
+    cp('-r', vtp.srcDir+'/data/labels/skycomp1', '.')
+
+    labeledDataToDB(l=dataset, n='negatives.yml', 
+            t=' '.join(str(t) for t in trainFrames),
+            T=' '.join(str(t) for t in testFrames))
+    vtp.makeVT('trainNet')
+    vtp.makeVT('basicDetector')
+    basicDetector = Command("util/basicDetector")
+    vtp.makeVT('buildNet')
+    basicDetector('-r', x, y, w, h, '-s', sz, '-n', n, dataset)
+
+    vtp.makeVT('detectionAccuracy')
+    detectionAccuracy = Command("util/detectionAccuracy")
+    out = detectionAccuracy(l=dataset, d='detections.pb', 
+            t=' '.join(str(t) for t in trainFrames),
+            T=' '.join(str(t) for t in testFrames))
+    return out
+    
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('command', choices=['submit', 'status', 'results'])
+args = parser.parse_args()
+
+if args.command == 'submit':
+    vtpalmetto.submit(vtp,task,[dict(dataset='skycomp1')])
+elif args.command == 'status':
+    vtpalmetto.printStatus(vtp)
 
